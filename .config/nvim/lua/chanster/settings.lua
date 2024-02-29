99c7a3983fb7b8620a2c7b2367137f3c29e0f1c6
Kevin Chan <kevin@chaster.net>
2024-02-29 14:20:58 -0600

    update vim settings

--- .config/nvim/lua/chanster/settings.lua
+++ .config/nvim/lua/chanster/settings.lua
@@ -3,3 +3,20 @@ vim.opt.termguicolors = true -- enable clolors
 
 vim.opt.hidden = true -- aollow buffer swap without saving
 vim.opt.number = true -- enable number line
+
+vim.opt.tabstop     = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth  = 4
+vim.opt.expandtab   = true -- expand tabs to spaces
+
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup   = false
+vim.opt.undodir  = os.getenv("HOME") .. "/.local/nvim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.colorcolumn = "80"
