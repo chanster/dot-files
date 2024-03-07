a8519324d484e7d3be3ac72087afa572a5806515
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-03-06 22:03:08 -0600

    remove buffer keymaps

--- .config/nvim/lua/chanster/keymaps.lua
+++ .config/nvim/lua/chanster/keymaps.lua
@@ -4,6 +4,3 @@ local map = vim.keymap.set
 -- Toggle auto-indenting for code paste
 map("n", "<F2>", ":set invpaste paste?<CR>", { desc = "toggle indenting"})
 
--- buffer
-map("n", "<C-[>", "<cmd>bp<CR>", { desc = "Load Previous Buffer" })
-map("n", "<C-]>", "<cmd>bn<CR>", { desc = "Load Next Buffer" })
