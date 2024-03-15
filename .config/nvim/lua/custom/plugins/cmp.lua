ea0da693a07cb241b1ea41c4adee589d1c7b580f
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-03-14 23:33:25 -0500

    update lsp configs

--- .config/nvim/lua/custom/plugins/cmp.lua
+++ .config/nvim/lua/custom/plugins/cmp.lua
@@ -14,6 +14,12 @@ return {
         local luasnip = require("luasnip")
         require("luasnip.loaders.from_vscode").lazy_load() -- load snippets from installe plugins
 
+        local has_words_before = function()
+          unpack = unpack or table.unpack
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
+
         cmp.setup({
             window = {
                 completion = cmp.config.window.bordered(),
@@ -28,17 +34,37 @@ return {
                 end
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-Up>"]    = cmp.mapping.select_prev_item(),
-                ["<C-Down>"]  = cmp.mapping.select_next_item(),
-                ["<C-[>"]     = cmp.mapping.scroll_docs(-5),
-                ["<C-]>"]     = cmp.mapping.scroll_docs(5),
-                ["<Tab>"]     = cmp.mapping.confirm({ select = false }),
+                ["<C-u>"]     = cmp.mapping.scroll_docs(-5),
+                ["<C-d>"]     = cmp.mapping.scroll_docs(5),
+                ["<CR>"]     = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+                        -- that way you will only jump inside the snippet region
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end),
                 ["<C-Space>"] = cmp.mapping.complete(), -- manually open cmp
                 ["<C-e>"]     = cmp.mapping.abort(), -- close cmp modal
             }),
             sources = cmp.config.sources({
-                { name = "luasnip" }, -- ultisnip snippets
                 { name = "nvim_lsp" }, -- lsp auto completions
+                { name = "luasnip" }, -- luasnip snippets
                 { name = "buffer" }, -- current open buffer
                 { name = "path" }, -- filesystem paths
             }),
--- .config/nvim/lua/custom/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/custom/plugins/lsp/lspconfig.lua
@@ -2,6 +2,7 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
+        "nvim-telescope/telescope.nvim",
         "williamboman/mason-lspconfig",
         { "antosha417/nvim-lsp-file-operations", config = true },  -- update file references on rename
     },
@@ -11,6 +12,7 @@ return {
     },
     config = function()
         local lspconfig = require("lspconfig")
+        local telescope = require("telescope.builtin")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
         local mason_lspconfig = require("mason-lspconfig")
         local capabilities = cmp_nvim_lsp.default_capabilities() -- enable autocompletion for language servers
@@ -24,19 +26,19 @@ return {
 
             -- set keybinds
             opts.desc = "Show LSP references"
-            map("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+            map("n", "gR", telescope.lsp_references, opts) -- show definition, references
 
             opts.desc = "Go to declaration"
             map("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
             opts.desc = "Show LSP definitions"
-            map("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+            map("n", "gd", telescope.lsp_definitions, opts) -- show lsp definitions
 
             opts.desc = "Show LSP implementations"
-            map("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+            map("n", "gi", telescope.lsp_implementations, opts) -- show lsp implementations
 
             opts.desc = "Show LSP type definitions"
-            map("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+            map("n", "gt", telescope.lsp_type_definitions, opts) -- show lsp type definitions
 
             opts.desc = "See available code actions"
             map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
@@ -82,10 +84,27 @@ return {
         lspconfig["ruff_lsp"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
+            init_options = {
+                settings = {
+                    args = {},
+                },
+            },
+        })
+        lspconfig["pylsp"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
+        lspconfig["pyright"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
         })
 
         -- terraform
         lspconfig["terraformls"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
+        lspconfig["tflint"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
         })
