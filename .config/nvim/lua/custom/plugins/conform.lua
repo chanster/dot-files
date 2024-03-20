63d46a9aa864df27667e0b7a0079ed1874b8593e
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-03-20 15:22:08 -0500

    feat: add formatters

---
+++ .config/nvim/lua/custom/plugins/conform.lua
@@ -0,0 +1,44 @@
+-- formatter
+return {
+    "stevearc/conform.nvim",
+    event = {
+        "BufReadPre",
+        "BufNewFile"
+    },
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            format_on_save = {
+                async = false,
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+            formatters_by_ft = {
+                -- https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
+                -- Conform will run multiple formatters sequentially
+                -- Use a sub-table to run only the first available formatter
+                python     = { "isort", "black" },
+                go         = { "gofmt" },
+                terraform  = { { "terraform_fmt", "hclfmt" } },
+                yaml       = { "prettier" },
+                json       = { "prettier" },
+                javascript = { { "prettierd", "prettier" } },
+                lua        = { "stylua" },
+            }
+        })
+
+        vim.keymap.set(
+            { "n", "v", },
+            "<Leader>mp",
+            function()
+                conform.format({
+                    async = false,
+                    timeout_ms = 500,
+                    lsp_fallback = true,
+                })
+            end,
+            { desc = "Make file Pretty" }
+        )
+    end
+}
