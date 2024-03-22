719619a09bde378c5b65d162b131a7740fa693c7
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-03-21 22:10:57 -0500

    fix: change telescope keymaps

--- .config/nvim/lua/custom/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/custom/plugins/lsp/lspconfig.lua
@@ -5,6 +5,7 @@ return {
         "nvim-telescope/telescope.nvim",
         "williamboman/mason-lspconfig",
         { "antosha417/nvim-lsp-file-operations", config = true }, -- update file references on rename
+        "folke/which-key.nvim",
     },
     event = {
         "BufReadPre",
@@ -15,6 +16,13 @@ return {
         local telescope = require("telescope.builtin")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
         local capabilities = cmp_nvim_lsp.default_capabilities() -- enable autocompletion for language servers
+        local wk = require("which-key")
+
+        wk.register({
+            ["<leader>g"] = {
+                name = "LSP...",
+            }
+        })
 
         -- load keybindings on lsp buffer
         local opts = { noremap = true, silent = true }
@@ -25,31 +33,33 @@ return {
 
             -- set keybinds
             opts.desc = "Show LSP references"
-            map("n", "gR", telescope.lsp_references, opts)               -- show definition, references
-            opts.desc = "Go to declaration"
-            map("n", "gD", vim.lsp.buf.declaration, opts)                -- go to declaration
+            map("n", "<leader>gR", telescope.lsp_references, opts)                 -- show definition, references
             opts.desc = "Show LSP definitions"
-            map("n", "gd", telescope.lsp_definitions, opts)              -- show lsp definitions
+            map("n", "<leader>gd", telescope.lsp_definitions, opts)                -- show lsp definitions
             opts.desc = "Show LSP implementations"
-            map("n", "gi", telescope.lsp_implementations, opts)          -- show lsp implementations
+            map("n", "<leader>gi", telescope.lsp_implementations, opts)            -- show lsp implementations
             opts.desc = "Show LSP type definitions"
-            map("n", "gt", telescope.lsp_type_definitions, opts)         -- show lsp type definitions
-            opts.desc = "See available code actions"
-            map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-            opts.desc = "Smart rename"
-            map("n", "<leader>rn", vim.lsp.buf.rename, opts)             -- smart rename
-            opts.desc = "Show buffer diagnostics"
-            map("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-            opts.desc = "Show line diagnostics"
-            map("n", "<leader>d", vim.diagnostic.open_float, opts)       -- show diagnostics for line
-            opts.desc = "Go to previous diagnostic"
-            map("n", "[d", vim.diagnostic.goto_prev, opts)               -- jump to previous diagnostic in buffer
-            opts.desc = "Go to next diagnostic"
-            map("n", "]d", vim.diagnostic.goto_next, opts)               -- jump to next diagnostic in buffer
-            opts.desc = "Show documentation for what is under cursor"
-            map("n", "K", vim.lsp.buf.hover, opts)                       -- show documentation for what is under cursor
+            map("n", "<leader>gt", telescope.lsp_type_definitions, opts)           -- show lsp type definitions
             opts.desc = "Restart LSP"
-            map("n", "<leader>rs", "<cmd>LspRestart<CR>", opts)          -- mapping to restart lsp if necessary
+            map("n", "<leader>gR", "<cmd>LspRestart<CR>", opts)                    -- mapping to restart lsp if necessary
+            opts.desc = "Show buffer diagnostics"
+            map("n", "<leader>gg", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+            -- vim lsp enhanced with telescope
+            opts.desc = "See available code actions"
+            map({ "n", "v" }, "<leader>gc", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+            opts.desc = "Smart rename"
+            map("n", "<leader>rn", vim.lsp.buf.rename, opts)               -- smart rename
+            opts.desc = "Go to declaration"
+            map("n", "<leader>gD", vim.lsp.buf.declaration, opts)          -- go to declaration
+            opts.desc = "Show line diagnostics"
+            map("n", "<leader>gd", vim.diagnostic.open_float, opts)        -- show diagnostics for line
+            opts.desc = "Go to previous diagnostic"
+            map("n", "[d", vim.diagnostic.goto_prev, opts)                 -- jump to previous diagnostic in buffer
+            opts.desc = "Go to next diagnostic"
+            map("n", "]d", vim.diagnostic.goto_next, opts)                 -- jump to next diagnostic in buffer
+            opts.desc = "Show documentation for what is under cursor"
+            map("n", "<leader>K", vim.lsp.buf.hover, opts)                 -- show documentation for what is under cursor
         end
 
         -- Change the Diagnostic symbols
