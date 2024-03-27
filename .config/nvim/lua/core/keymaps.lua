52d0ddce5e894ba21435c63bba743c158db49793
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-03-27 14:49:14 -0500

    feat: toogle relative numbers

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -2,5 +2,12 @@ vim.g.mapleader = " " -- update <Leader>
 
 local map = vim.keymap.set
 -- Toggle auto-indenting for code paste
-map("n", "<F2>", ":set invpaste paste?<CR>", { desc = "toggle indenting"})
+map("n", "<F2>", ":set invpaste paste?<CR>", { desc = "toggle indenting" })
 
+-- Toggle relative numbers
+map({ "n", "v" }, "<C-r>",
+    function()
+        vim.cmd("set relativenumber!")
+    end,
+    { desc = "Toggle Line numbers" }
+)
