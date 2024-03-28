a9dd47bd39a9bfb7a498dce31a99fff865f755b6
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-03-28 09:33:36 -0500

    Revert "chore: change notification"
    This reverts commit c568cb9d076fa96b7a70d6c3da174da42a2ac869.

--- .config/nvim/lua/custom/lazy.lua
+++ .config/nvim/lua/custom/lazy.lua
@@ -2,13 +2,9 @@ require("lazy").setup(
     {
         { import = "custom.plugins" },
         { import = "custom.plugins.lsp" },
-        { import = "custom.plugins.dap" },
         { import = "custom.plugins.linters" },
     },
     {
-        -- defaults = {
-        --     lazy = true
-        -- },
         checker = {
             enabled = true,
             notify = true
