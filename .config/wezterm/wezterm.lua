eafc27bc3492442becf6265664753cb45d38823c
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-04-05 16:37:03 -0500

    feat: add word jump on mac

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,5 +6,12 @@ config.font = wezterm.font 'FiraCode Nerd Font'
 
 config.window_background_opacity = 0.999 -- workaround for macos stutter
 
+config.keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    { key = "LeftArrow",  mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
+}
+
 -- and finally, return the configuration to wezterm
 return config
