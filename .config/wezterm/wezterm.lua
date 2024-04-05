fa77e48bfd5ec02c18798121cefd6bb7772dfe3c
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-04-05 16:31:51 -0500

    fix: font loading

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Tokyo Night'
-config.font "FiraCode Nerd Font"
+config.font = wezterm.font 'FiraCode Nerd Font'
 
 config.window_background_opacity = 0.999 -- workaround for macos stutter
 
