d2c28e3d615f03ce51dfe4d085f19f3e9eff3362
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-04-05 20:04:50 -0500

    feat: enable treesitter check

--- .config/nvim/lua/custom/plugins/autopairs.lua
+++ .config/nvim/lua/custom/plugins/autopairs.lua
@@ -4,7 +4,11 @@ return {
     event = { "InsertEnter" },
     config = function()
         local apairs = require('nvim-autopairs')
+
         apairs.setup({
+            check_ts = true,                  -- enable treesitter
+            enable_check_bracket_line = true, -- check for closing bracket
+            fast_wrap = {},
             disable_filetype = {
                 "TelescopePrompt",
                 "spectre_panel",
