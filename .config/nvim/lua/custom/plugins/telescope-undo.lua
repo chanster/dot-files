edebff493155752f61afe687040984106a19882c
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-04-05 20:06:32 -0500

    chore: make undo preview smaller

--- .config/nvim/lua/custom/plugins/telescope-undo.lua
+++ .config/nvim/lua/custom/plugins/telescope-undo.lua
@@ -19,7 +19,7 @@ return {
                 side_by_side = true,
                 layout_strategy = "vertical",
                 layout_config = {
-                    preview_height = 0.8,
+                    preview_height = 0.7,
                 },
             },
         },
