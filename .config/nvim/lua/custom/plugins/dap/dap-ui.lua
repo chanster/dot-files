eb07c88b09f887d139f405e13efa62b0a3a19d3f
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-04-07 03:14:48 -0500

    chore: restructure code

--- .config/nvim/lua/custom/plugins/dap/dap-ui.lua
+++ .config/nvim/lua/custom/plugins/dap/dap-ui.lua
@@ -1,31 +1,33 @@
-return {
-    "rcarriga/nvim-dap-ui",
-    dependencies = {
-        "mfussenegger/nvim-dap", -- dap client
-        "nvim-neotest/nvim-nio"  -- async library
-    },
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
-        local map = vim.keymap.set
+local M = { "rcarriga/nvim-dap-ui" }
 
-        dapui.setup()
-
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-
-        map({ "n" }, "<leader>du", function() require("dapui").toggle({}) end, { desc = "[D]AP [U]I toggle" })
-        map({ "n", "v" }, "<leader>de", function() require("dapui").eval() end,
-            { desc = "[D]AP UI [E]valuate expression" })
-    end
+M.dependencies = {
+    "mfussenegger/nvim-dap", -- dap client
+    "nvim-neotest/nvim-nio"  -- async library
 }
+
+M.config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+    local map = vim.keymap.set
+
+    dapui.setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+    end
+
+    map({ "n" }, "<leader>du", function() require("dapui").toggle({}) end, { desc = "[D]AP [U]I toggle" })
+    map({ "n", "v" }, "<leader>de", function() require("dapui").eval() end,
+        { desc = "[D]AP UI [E]valuate expression" })
+end
+
+return M
