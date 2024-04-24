69b746b9e207e57027ec9d9deaa24a82c59a0aa9
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-04-24 12:22:30 -0500

    feat: seamless window motion between tmux and supported apps

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -18,16 +18,6 @@ map({ "n", "v" }, "<C-n>",
 map("n", "<C-s>", ":w<CR>", { desc = "Save File" })
 map("i", "<C-s>", "<Esc>:w<CR>i", { desc = "Save File" })
 
--- Move to window using the <ctrl> hjkl keys
-map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
-map("n", "<C-Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
-map("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
-map("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
-map("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
-
 -- move buffers
 map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
---
+++ .config/nvim/lua/custom/plugins/nvim-tmux-navigation.lua
@@ -0,0 +1,21 @@
+local M = { "alexghergh/nvim-tmux-navigation" }
+
+M.config = function()
+    local tnav = require("nvim-tmux-navigation")
+    local map = vim.keymap.set
+
+    tnav.setup({
+        disable_when_zoomed = true
+    })
+
+    map('n', "<C-h>", tnav.NvimTmuxNavigateLeft, { desc = "Go to Left Pane" })
+    map('n', "<C-j>", tnav.NvimTmuxNavigateDown, { desc = "Go to Down Pane" })
+    map('n', "<C-k>", tnav.NvimTmuxNavigateUp, { desc = "Go to Up Pane" })
+    map('n', "<C-l>", tnav.NvimTmuxNavigateRight, { desc = "Go to Right Pane" })
+    map('n', "<C-Left>", tnav.NvimTmuxNavigateLeft, { desc = "Go to Left Pane" })
+    map('n', "<C-Down>", tnav.NvimTmuxNavigateDown, { desc = "Go to Down Pane" })
+    map('n', "<C-Up>", tnav.NvimTmuxNavigateUp, { desc = "Go to Up Pane" })
+    map('n', "<C-Right>", tnav.NvimTmuxNavigateRight, { desc = "Go to Right Pane" })
+end
+
+return M
