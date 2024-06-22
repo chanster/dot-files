69263e16e55c94692fc0323766366e1670ac54eb
Kevin Chan <232767+chanster@users.noreply.github.com>
2024-06-22 16:20:07 -0500

    fix(neovim): set cursor to after char on insert save

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -9,14 +9,15 @@ map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsea
 
 -- Toggle relative numbers
 map({ "n", "v" }, "<C-n>",
-    function()
-        vim.cmd("set relativenumber!")
-    end,
-    { desc = "Toggle relative line numbers" }
+	function()
+		vim.cmd("set relativenumber!")
+	end,
+	{ desc = "Toggle relative line numbers" }
 )
 
+-- ctrl + s to save file
 map("n", "<C-s>", ":w<CR>", { desc = "Save File" })
-map("i", "<C-s>", "<Esc>:w<CR>i", { desc = "Save File" })
+map("i", "<C-s>", "<Esc>:w<CR>a", { desc = "Save File" })
 
 -- move buffers
 map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
